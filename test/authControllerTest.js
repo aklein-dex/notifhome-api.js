e8746cac9041364b0e0c4ec9abe8e51d55b0eb8c
Alex <alexandre.klein@tanukisoftware.com>
2018-04-25 16:59:50 +0900

    add test libraries mocha and chai

---
+++ test/authControllerTest.js
@@ -0,0 +1,36 @@
+// https://scotch.io/tutorials/test-a-node-restful-api-with-mocha-and-chai
+
+//During the test the env variable is set to test
+process.env.NODE_ENV = 'test';
+
+let mongoose = require("mongoose");
+let User = require('../app/models/user');
+
+//Require the dev-dependencies
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require('../notifhome');
+let should = chai.should();
+
+chai.use(chaiHttp);
+//Our parent block
+describe('Users', () => {
+  beforeEach((done) => { //Before each test we empty the database
+    User.remove({}, (err) => { 
+      done();         
+    });
+  });
+    
+  describe('POST /auth/signin', () => {
+    it('it should sign in and return a token', (done) => {
+      chai.request(server)
+          .post('/auth/signin')
+          .end((err, res) => {
+            res.should.have.status(200);
+            
+            done();
+          });
+    });
+  });
+
+});
