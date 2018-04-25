edc935b522147f7142250e9b48eea55457f3717c
Alex <alexandre.klein@tanukisoftware.com>
2018-04-25 15:41:10 +0900

    add pre save for models to set the created_at and updated_at date

--- app/models/device.js
+++ app/models/device.js
@@ -10,6 +10,21 @@ var deviceSchema = new Schema({
   updated_at: Date
 });
 
+// on every save, add the date
+deviceSchema.pre('save', function(next) {
+  // get the current date
+  var currentDate = new Date();
+
+  // change the updated_at field to current date
+  this.updated_at = currentDate;
+
+  // if created_at doesn't exist, add to that field
+  if (!this.created_at)
+    this.created_at = currentDate;
+
+  next();
+});
+
 var Device = mongoose.model('Device', deviceSchema);
 
 module.exports = Device;
--- app/models/notification.js
+++ app/models/notification.js
@@ -10,6 +10,21 @@ var notificationSchema = new Schema({
   updated_at: Date
 });
 
+// on every save, add the date
+notificationSchema.pre('save', function(next) {
+  // get the current date
+  var currentDate = new Date();
+
+  // change the updated_at field to current date
+  this.updated_at = currentDate;
+
+  // if created_at doesn't exist, add to that field
+  if (!this.created_at)
+    this.created_at = currentDate;
+
+  next();
+});
+
 var Notification = mongoose.model('Notification', notificationSchema);
 
 module.exports = Notification;
